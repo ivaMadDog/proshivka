314ed77c8abf8af1881c254019d2841e1ab621c1
Ivan Prikhodko <ivan-prikhodko@yandex.ru>
2013-06-27 01:27:01 +0300

    First Commit

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -1,5 +1,10 @@
 <?php
 /**
+ * Application level Controller
+ *
+ * This file is application-wide controller file. You can put all
+ * application-wide controller-related methods here.
+ *
  * PHP 5
  *
  * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
@@ -11,8 +16,20 @@
  *
  * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
  * @link          http://cakephp.org CakePHP(tm) Project
- * @package       app
- * @since         CakePHP(tm) v 0.10.0.1076
+ * @package       app.Controller
+ * @since         CakePHP(tm) v 0.2.9
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
-require 'webroot' . DIRECTORY_SEPARATOR . 'index.php';
+App::uses('Controller', 'Controller');
+
+/**
+ * Application Controller
+ *
+ * Add your application-wide methods in the class below, your controllers
+ * will inherit them.
+ *
+ * @package		app.Controller
+ * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
+ */
+class AppController extends Controller {
+}
